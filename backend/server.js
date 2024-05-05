55579278c3d8eefdc9796d79e8dac71d5ccc9b99
azfcf <azfcf@umsystem.edu>
2024-05-04 22:44:52 -0500

    remove unnecessary import

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,6 @@ const cors = require('cors');
 const express = require('express');
 const bcrypt = require('bcrypt');
 const { body, validationResult } = require('express-validator')
-const passport = require('passport')
 
 const app = express()
 
